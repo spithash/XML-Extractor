d8e462ad73b8b76d0feb74d78afc62a9a16a289f
spithash <spithash@vulpix.skynet>
2023-02-01 22:34:26 +0200

    Added progress bar and moved/created variables

--- xml-extractor.py
+++ xml-extractor.py
@@ -1,16 +1,28 @@
 import requests
 import re
+import time
+import sys
 
-# Set the user agent to the latest Mozilla Firefox
+def progress_bar(iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filled_length = int(length * iteration // total)
+    bar = fill * filled_length + '-' * (length - filled_length)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
+    sys.stdout.flush()
+    if iteration == total: 
+        print()
+
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/101.0'}
+url = "https://www.example.com/productsxml/"
+desired_category = "My category name"
+selector = "<level3_category_description>" + desired_category + "</level3_category_description>"
 
-# Request the XML from the URL
-response = requests.get("https://www.example.com/productsxml/", headers=headers)
+print("Fetching XML data...\n")
 
-# Get the status code
-status_code = response.status_code
+url_response = requests.get(url, headers=headers)
 
-# Color the status code message based on the value
+status_code = url_response.status_code
+
 if status_code >= 200 and status_code < 300:
     print("\033[32mStatus code:", status_code, "\033[0m")
 elif status_code >= 300 and status_code < 400:
@@ -18,27 +30,21 @@ elif status_code >= 300 and status_code < 400:
 else:
     print("\033[31mStatus code:", status_code, "\033[0m")
 
-# Get the XML content
-xml = response.text
+xml = url_response.text
 
-# Find all entries (products) in the XML
 entries = re.findall("<entry>.*?</entry>", xml, re.DOTALL)
 
-# Initialize the count of all products and the count of found products
 product_count = len(entries)
 found_product_count = 0
-
-# The desired level3_category_description
-desired_category = "My category name"
-
-# Search for the desired level3_category_description in each entry
 matching_entries = []
-for entry in entries:
-    if "<level3_category_description>" + desired_category + "</level3_category_description>" in entry:
+
+for i, entry in enumerate(entries):
+    if selector in entry:
         found_product_count += 1
         matching_entries.append(entry)
+    progress_bar(i + 1, product_count, prefix = 'Loading:', suffix = 'Complete', length = 50)
+    time.sleep(0.001)
 
-# Write the matching entries into the output.xml file
 with open("output.xml", "w", encoding="UTF-8") as f:
     f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
     f.write("<product_results>\n")
@@ -46,7 +52,6 @@ with open("output.xml", "w", encoding="UTF-8") as f:
         f.write("  " + entry + '\n')
     f.write("</product_results>")
 
-# Print the result
 print("\033[1mNumber of products in the XML:\033[0m", product_count)
 print("\033[36mNumber of found products:\033[0m", found_product_count)
 
