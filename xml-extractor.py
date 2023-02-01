556ef6cbdafb7331db87ae3a257d50460bd605c2
spithash <spithash@vulpix.skynet>
2023-02-01 23:16:29 +0200

    minor changes

--- xml-extractor.py
+++ xml-extractor.py
@@ -12,7 +12,7 @@ def progress_bar(iteration, total, prefix = '', suffix = '', decimals = 1, lengt
     if iteration == total: 
         print()
 
-# Variables
+# Set request header User-Agent
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/101.0'}
 
 # Set the XML url
