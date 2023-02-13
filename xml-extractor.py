f122c21b2e05dfbb605c0fbf5ae64dcdb4193ce7
spithash <spithash@tuta.io>
2023-02-13 12:16:29 +0200

    minor changes

--- xml-extractor.py
+++ xml-extractor.py
@@ -3,6 +3,7 @@ import re
 import time
 import sys
 
+# The progress bar
 def progress_bar(iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
     filled_length = int(length * iteration // total)
@@ -18,10 +19,10 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Ge
 # Set the XML url
 url = "https://www.example.com/productsxml/"
 
-# Set the category name
+# Set the category name. It's how you select.
 desired_category = re.compile("My category name.*")
 
-# Set element prefix and suffix
+# Set element prefix and suffix. Where it starts and where it stops searching.
 selectorprefix = "<level3_category_description>"
 selectorsuffix = "</level3_category_description>"
 
@@ -33,7 +34,7 @@ print("Fetching XML data...\n")
 # Request the XML from the URL
 url_response = requests.get(url, headers=headers)
 
-# Get the status code
+# Get the HTTP status code
 status_code = url_response.status_code
 
 # Color the HTTP status code message based on the value
@@ -47,10 +48,10 @@ else:
 # Get the XML content
 xml = url_response.text
 
-# Find all entries (products) in the XML
+# Find all entries (products for example) in the XML
 entries = re.findall("<entry>.*?</entry>", xml, re.DOTALL)
 
-# Count total products and then count matching products
+# Count total products and then count the matching products it finds
 product_count = len(entries)
 found_product_count = 0
 matching_entries = []
@@ -63,7 +64,7 @@ for i, entry in enumerate(entries):
     progress_bar(i + 1, product_count, prefix = 'Loading:', suffix = 'Complete', length = 50)
     time.sleep(0.001)
 
-# Write the matching entries into the output.xml file
+# Write the matching entries into the desired .xml file
 with open(output_file_name, "w", encoding="UTF-8") as f:
     f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
     f.write("<product_results>\n")
@@ -71,6 +72,7 @@ with open(output_file_name, "w", encoding="UTF-8") as f:
         f.write("  " + entry + '\n')
     f.write("</product_results>")
 
+# Output
 print("\033[1mNumber of products in the XML:\033[0m", product_count)
 print("\033[36mNumber of found products:\033[0m", found_product_count)
 print("\033[32mOutput written to file: " + output_file_name + "\033[0m")
